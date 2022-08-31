fc2dfe72ddbc2c98edac8ee07ba2500c86d829c0
koduka <koduka0715@gmail.com>
2022-08-31 16:22:11 +0900

    プロジェクト作成

---
+++ vetur.config.js
@@ -0,0 +1,12 @@
+// https://vuejs.github.io/vetur/reference/
+module.exports = {
+    settings: {
+      "vetur.useWorkspaceDependencies": true,
+      "vetur.experimental.templateInterpolationService": true
+    },
+    projects: [
+      'strapi/app',
+      'web',
+    ]
+  }
+  
